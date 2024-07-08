bbd3a4451391ee61a9f2176f566218684bf5a89e
Kevin <kevhoang97@gmail.com>
2024-07-08 14:54:41 -0400

    created readme generator

--- index.js
+++ index.js
@@ -56,3 +56,56 @@ inquirer
         },
 
     ])
+
+    .then((data) => {
+        var badgeLink = "";
+        if (`${data.license}` === "mit") {
+            badgeLink = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+        }
+        else if (`${data.license}` === "apache") {
+            badgeLink = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+        }
+        else {
+            badgeLink = "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+        }
+
+        console.log(data.license)
+        console.log(badgeLink)
+        const myReadme =`# ${data.title}
+
+${badgeLink}
+## Description
+${data.discription}
+
+## Table of Content
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Test](#tests)
+- [Question](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This application license is covered by ${data.license}
+
+## Contributing
+${data.contribute}
+
+## Tests
+${data.test}
+
+## Questions
+You can see more of my projects on my github page: https://github.com/${data.username}/
+or contact me via email at: ${data.email}
+`;
+
+    fs.writeFile('README.md', JSON.stringify(myReadme), (err) =>
+        err ? console.error(err) : console.log('created file'));
+})
+
